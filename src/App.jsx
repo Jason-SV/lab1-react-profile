c72f46436d126ef46f4a84736f4171eb881d31ac
Jason <jasonitmx@gmail.com>
2026-01-05 13:29:46 +0700

    feat: add ProfileCard component and pass data via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ProfileCard from './components/ProfileCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <h1>My First React App</h1>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <h1>My Team Portfolio</h1>
+      <ProfileCard
+        name={"Jason Chawsuan"}
+        role={"Student @ CEDT"}
+        bio={"I am a very general student with a little bit skill in backend."}
+      />
+      <ProfileCard
+        name={"Robloxian Stevens"}
+        role={"Student @ SOMEPLACE"}
+        bio={"I come from Roblox."}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
